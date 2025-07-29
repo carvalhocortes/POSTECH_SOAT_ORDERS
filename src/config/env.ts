39dd6093f99e39a9524d052b928e39af56947ce7
CarvalhoCortes <carvalhocortes@gmail.com>
2025-07-29 13:08:40 -0300

    fix: remover decodificação base64 das variáveis de ambiente AWS

--- src/config/env.ts
+++ src/config/env.ts
@@ -25,9 +25,9 @@ export const env = envSchema.validateSync({
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   AWS_REGION: process.env.AWS_REGION,
-  AWS_ACCESS_KEY_ID: Buffer.from(process.env.AWS_ACCESS_KEY_ID ?? '', 'base64').toString('utf-8'),
-  AWS_SECRET_ACCESS_KEY: Buffer.from(process.env.AWS_SECRET_ACCESS_KEY ?? '', 'base64').toString('utf-8'),
-  AWS_SESSION_TOKEN: Buffer.from(process.env.AWS_SESSION_TOKEN ?? '', 'base64').toString('utf-8'),
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  AWS_SESSION_TOKEN: process.env.AWS_SESSION_TOKEN,
 });
 
 export type EnvConfig = yup.InferType<typeof envSchema>;
